01ffbba4f79ff4cff994886964eabd39a91e2f06
Kondo Takeo <takeover@gmail.com>
2014-01-28 00:25:46 +0900

    FizzBuzz.

---
+++ 20140127/fizzbuzz.rb
@@ -0,0 +1,36 @@
+class Fizzbuzz
+  def initialize(min, max)
+    @min = min
+    @max = max
+  end
+
+  def fizz? num
+    return num % 3 == 0
+  end
+
+  def buzz? num
+    return num % 5 == 0
+  end
+
+  def fizzbuzz num
+    if fizz? num and buzz? num
+      p "FizzBuzz"
+    elsif fizz? num
+      p "Fizz"
+    elsif buzz? num
+      p "Buzz"
+    else
+      p num
+    end
+  end
+
+  def run
+    for i in @min..@max
+      fizzbuzz i
+    end
+  end
+end
+
+
+fb = Fizzbuzz.new(1, 100)
+fb.run
