f432833254275b77df6756618b73c545e5aafe0e
Kondo Takeo <takeover@gmail.com>
2014-05-08 12:06:01 +0900

    FizzBuzz with Python.

---
+++ 20140428/fizzbuzz.py
@@ -0,0 +1,15 @@
+def toStr(i):
+    if i % 15 == 0:
+        return "FizzBuzz"
+    elif i % 3 == 0:
+        return "Fizz"
+    elif i % 5 == 0:
+        return "Buzz"
+    else:
+        return i
+
+def fizzBuzz(min, max):
+    for i in range(min, max+1):
+        print(toStr(i))
+
+fizzBuzz(1, 100)
