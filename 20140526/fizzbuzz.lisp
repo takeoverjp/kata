867cb2e8a433515231be688b06e305ce2b0e536b
Kondo Takeo <takeover@gmail.com>
2014-05-28 12:24:27 +0900

    FizzBuzz with lisp.

---
+++ 20140526/fizzbuzz.lisp
@@ -0,0 +1,12 @@
+(defun fizzbuzz (min max)
+  (if (<= min max)
+      (progn
+        (cond
+         ((eq (mod min 15) 0) (format t "FizzBuzz~%"))
+         ((eq (mod min 3)  0) (format t "Fizz~%"))
+         ((eq (mod min 5)  0) (format t "Buzz~%"))
+         (t                   (format t "~A~%" min)))
+        (fizzbuzz (1+ min) max))))
+
+
+(fizzbuzz 1 100)
