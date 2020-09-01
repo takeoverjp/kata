1260bdfcec014e1b8f0851128168d9e284bc7ec8
Kondo Takeo <takeoverjp@gmail.com>
2020-09-02 00:14:04 +0900

    Pass all test of Bowring.

--- 20200901/bowling.cpp
+++ 20200901/bowling.cpp
@@ -13,6 +13,14 @@ public:
     } else if (m_score == 10){
       m_spare = true;
     }
+    m_pins.push_back(first);
+    if (second)
+      m_pins.push_back(second);
+    if (third)
+      m_pins.push_back(third);
+  }
+  std::vector<int> pins() {
+    return m_pins;
   }
   int score(const int next = 0, const int next_next = 0) const {
     if (m_strike) {
@@ -26,6 +34,7 @@ public:
   }
 
 private:
+  std::vector<int> m_pins;
   int m_score;
   bool m_strike;
   bool m_spare;
@@ -40,8 +49,20 @@ public:
   }
   int score() {
     int sum = 0;
-    for (const auto frame : m_frames) {
-      sum += frame.score();
+    for (u_int i = 0; i < m_frames.size(); i++) {
+      int next = 0;
+      int next_next = 0;
+      if (i < m_frames.size() - 1) {
+        std::vector<int> pins = m_frames[i+1].pins();
+        next = pins[0];
+        if (pins.size() >= 2) {
+          next_next = pins[1];
+        } else if (i < m_frames.size() - 2) {
+          std::vector<int> pins = m_frames[i+2].pins();
+          next_next = pins[0];
+        }
+      }
+      sum += m_frames[i].score(next, next_next);
     }
     return sum;
   }
@@ -91,4 +112,39 @@ TEST(BowlingTest, GameSpare) {
   g.addFrame(Frame(3, 4));
 
   ASSERT_EQ(20, g.score());
+}
+
+TEST(BowlingTest, GameStrike) {
+  Game g;
+  g.addFrame(Frame(10));
+  g.addFrame(Frame(3, 4));
+
+  ASSERT_EQ(24, g.score());
+}
+
+TEST(BowlingTest, GameTurkey) {
+  Game g;
+  g.addFrame(Frame(10));
+  g.addFrame(Frame(10));
+  g.addFrame(Frame(10));
+
+  ASSERT_EQ(60, g.score());
+}
+
+TEST(BowlingTest, GameLastSpare) {
+  Game g;
+  for (int i = 0; i < 9; i++)
+    g.addFrame(Frame(10));
+  g.addFrame(Frame(1, 9, 10));
+
+  ASSERT_EQ(300, g.score());
+}
+
+TEST(BowlingTest, GameFullScore) {
+  Game g;
+  for (int i = 0; i < 9; i++)
+    g.addFrame(Frame(10));
+  g.addFrame(Frame(10, 10, 10));
+
+  ASSERT_EQ(300, g.score());
 }
